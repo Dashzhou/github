03e3aa0fe2794bebe9aa5b4982b43304f7cc8ac4
zyh <zyh@qq.com>
2022-02-28 15:14:04 +0800

    second commit

--- src/main/java/zyh/test/zyhTest.java
+++ src/main/java/zyh/test/zyhTest.java
@@ -6,5 +6,6 @@ package zyh.test;
 public class zyhTest {
     public static void main(String[] args) {
         System.out.println("GitHub 你好啊");
+        System.out.println("GitHub HAHA 你好啊");
     }
 }
