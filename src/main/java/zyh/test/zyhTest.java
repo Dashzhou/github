88eb1371e896c8476f9b116cf31fdebb620e7b73
zyh <zyh@qq.com>
2022-02-28 15:21:03 +0800

    hot-fix commit

--- src/main/java/zyh/test/zyhTest.java
+++ src/main/java/zyh/test/zyhTest.java
@@ -7,5 +7,6 @@ public class zyhTest {
     public static void main(String[] args) {
         System.out.println("GitHub 你好啊");
         System.out.println("GitHub HAHA 你好啊");
+        System.out.println("Hot-fix");
     }
 }
